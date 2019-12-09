f6059ac9bb1fbf2f783719e4a5deec7e909d27e5
Alexey Zamora <alexeyzamora@ZAMORA-Alexey-iMac27.local>
2019-12-09 11:56:19 +0100

    Remove profile from prepare_templates.sh

--- prepare_templates.sh
+++ prepare_templates.sh
@@ -3,11 +3,11 @@
 if [[ $# -ge 1 ]]; then
   echo "The bucket specified is: $1"
   echo "Starting copy of all nested templates to bucket to s3://$1"
-  aws s3 cp infrastructure/ s3://$1 --recursive --profile WIP  
+  aws s3 cp infrastructure/ s3://$1 --recursive  
 else 
   echo "Usind default bucket: nested-templates-wip"
   echo "Starting copy of all nested templates to bucket to s3://nested-templates-wip"
-  aws s3 cp infrastructure/ s3://nested-templates-wip --recursive --profile WIP  
+  aws s3 cp infrastructure/ s3://nested-templates-wip --recursive  
 fi
 
 
